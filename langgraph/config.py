56b81c9d49f63cc3c2e7718151309378e6b52374
Babar Raheem <baberraheem055@gmail.com>
2026-07-07 22:32:24 +0500

    create two structure folders

--- langgraph/config.py
+++ langgraph/config.py
@@ -1,11 +1,14 @@
+import os
 from pydantic_settings import BaseSettings
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Settings(BaseSettings):
 
     Groq_api_key: str
+    TAVILY_API_KEY: str
 
-    model_config = {"env_file": ".env"}
+    model_config = {"env_file": os.path.join(current_dir, ".env")}
 
 
 
